103effad2ed1722e779a9e6648fb359d03906341
romeron29 <nes.romero.17@gmail.com>
2025-10-22 00:43:09 -0600

    test: prueba de cambios en git

--- index.php
+++ index.php
@@ -3,13 +3,9 @@
     require_once __DIR__ . "/controllers/UserController.php";
     require_once __DIR__ . "/controllers/AuthController.php";
     require_once __DIR__ . "/config/db.php";*/
-
+    
     $action = $_GET["action"] ?? "login";
     switch ($action) {
-        case "create":
-            // show form and process create if POST
-            $controller->listProvinces();            
-            break;
         case "save":
             $userController->saveUser();
             break;
@@ -29,6 +25,7 @@
             $userController->disableUser();
             break;
         case "auth":
+            //Validate user credentials
             $authController->validateUser();
             break;
         case "logout":
@@ -36,6 +33,7 @@
             break;
         case "login":
         default:
+            
             require 'views/login.php';
             break;
     }
